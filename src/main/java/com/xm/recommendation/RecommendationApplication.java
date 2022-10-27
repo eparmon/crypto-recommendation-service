35b8089d948140fa1f2d9610521bb5bd4331a8a3
Gene <eparmon@tuta.io>
2022-10-27 17:55:35 +0400

    Add OpenAPI documentation to the GET /cryptocurrencies endpoint

--- src/main/java/com/xm/recommendation/RecommendationApplication.java
+++ src/main/java/com/xm/recommendation/RecommendationApplication.java
@@ -1,9 +1,22 @@
 package com.xm.recommendation;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Crypto Recommendation Service",
+        description = "A service that recommends a cryptocurrency to invest in",
+        contact = @Contact(name = "Gene Parmon", url = "https://t.me/eparmon", email = "eparmon@tuta.io"),
+        license = @License(name = "Unlicense", url = "https://unlicense.org/"),
+        version = "1.0.0"),
+        tags = @Tag(name = "Crypto")
+)
 public class RecommendationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/xm/recommendation/web/config/WebConfig.java
+++ src/main/java/com/xm/recommendation/web/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.xm.recommendation.web.config;
 
 import com.xm.recommendation.web.handler.GetCryptocurrenciesRequestHandler;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RequestPredicates;
@@ -12,6 +13,10 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class WebConfig {
 
     @Bean
+    @RouterOperation(
+            beanClass = GetCryptocurrenciesRequestHandler.class,
+            beanMethod = "getCryptocurrenciesSortedByNormalizedRange",
+            produces = "application/json")
     public RouterFunction<ServerResponse> routes(GetCryptocurrenciesRequestHandler getCryptocurrenciesRequestHandler) {
         return RouterFunctions.route(RequestPredicates.GET("/cryptocurrencies"),
                 getCryptocurrenciesRequestHandler::getCryptocurrenciesSortedByNormalizedRange);
